b6fda041e4d51322a1dc67cc7ebe06f9e97767fd
RMLuis1 <94114192+RMLuis1@users.noreply.github.com>
2022-09-28 14:03:12 -0300

    Proyecto

---
+++ index.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementsByClassName("menu-button")[0];
+
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+menuButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
